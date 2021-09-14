225ed14e8dba86c809e9cfacd85dc0f42bcabc01
zengliwei <zengliwei@163.com>
2021-09-14 17:39:31 +0800

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -10,8 +10,10 @@ for /f "tokens=1,2 delims==" %%i in (.\.env) do (
         set project=%%j
     ) else if %%i == DOMAIN (
         set domain=%%j
-    ) else if %%i == RESTART (
-        set restart=%%j
+    ) else if %%i == DB_USER (
+        set dbUser=%%j
+    ) else if %%i == DB_PSWD (
+        set dbPass=%%j
     )
 )
 
@@ -20,7 +22,7 @@ for /f "tokens=1,2 delims==" %%i in (.\.env) do (
 ::
 for /f "skip=1" %%c in ('docker ps -a --filter "name=%project%_web"') do (
     if not %%c == '' (
-        call start-project "%domain%" "%restart%"
+        call start-project "%domain%"
         exit
     )
 )
@@ -28,6 +30,6 @@ for /f "skip=1" %%c in ('docker ps -a --filter "name=%project%_web"') do (
 call add-host-record "%domain%"
 call create-proxy-config "%project%" "%domain%"
 call docker-compose up --no-recreate -d
-call config-phpmyadmin "%project%"
+call config-phpmyadmin "%project%" "%dbUser%" "%dbPass%"
 call decompress-packets "%project%"
 call start-project "%domain%"
